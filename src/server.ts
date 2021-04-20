e38d5d24aac856696846789e79c3d906cc33e5b1
jarbson costa <jarbsonfc@gmail.com>
2021-04-19 22:07:18 -0300

    criando projeto

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, {Request, Response} from 'express'
+
+
+const app = express()
+
+app.get('/', (request: Request, response: Response) => {
+    return response.send('Hello World')
+})
+
+
+app.listen(3333, () => {
+    console.log('servidor rodando', 3333)
+})
